48bf785a4db54bc76f9d69351b4887a0c2262dfc
buenorafa <rafaelbuenooo@outlook.com>
2023-03-22 23:47:37 -0300

    exe36

--- Exercícios/exe36.js
+++ Exercícios/exe36.js
@@ -30,19 +30,19 @@ function cipher(text, num) {
   ];
   const lastIndex = alphabet.length - 1;
   text = text.split("");
-
-  for (let letra of text) {
-    if (alphabet.indexOf(letra) + num > lastIndex) {
-      const rotIndex = alphabet.indexOf(letra) + num - lastIndex - 1;
+  // P/ cada letra do parâmetro de texto, será verificado se a soma do index da letra com o parâmetro numérico é maior que o index da letra 'z', se for maior subtraímos dessa soma o index de 'z' menos 1 (pois o array começa no indice 0), caso contrário basta adicionar ao index da letra o parâmetro numérico
+  for (let letter of text) {
+    if (alphabet.indexOf(letter) + num > lastIndex) {
+      const rotIndex = alphabet.indexOf(letter) + num - lastIndex - 1;
       result += alphabet[rotIndex];
     } else {
-      result += alphabet[alphabet.indexOf(letra) + num];
+      result += alphabet[alphabet.indexOf(letter) + num];
     }
   }
   return result;
 }
 
-console.log(cipher("abc", 2)); //cde
-console.log(cipher("xyz", 2)); //zab
-console.log(cipher("abc", 13)); //	nop
-console.log(cipher("xyz", 13)); //	klm
+console.log(cipher("abc", 2)); //  cde
+console.log(cipher("xyz", 2)); //  zab
+console.log(cipher("abc", 13)); //  nop
+console.log(cipher("xyz", 13)); //  klm
